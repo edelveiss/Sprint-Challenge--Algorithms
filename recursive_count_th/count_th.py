f97dc44bff65762aefc08c9ae8d1ceee136637d0
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-10-09 20:14:35 -0700

    robot sort function is completed

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,10 +6,13 @@ Your function must utilize recursion. It cannot contain any loops.
 #RunTime complexity: O(n)
 #Space complexity: O(n)
 def count_th(word):
+    #base case
     if len(word) == 0:
        return 0
     if word[:2] == 'th':
+        #if there is 'th' in the word the counter is increased by one and move toward the base case
         return count_th(word[1:]) + 1
+    #move toward the base case
     return count_th(word[1:])
 
 print("th: ", count_th("thhthokTHhhth")) # 3 th
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,17 +96,71 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        #I will use a something like a bubble sort. When swap items the light is turned on. 
+        #If one or more swapes performed - repeat process.
+        # Bubble sort O(n^2)
+        self.set_light_on()
+        #if there is no light_on while loop is terminated
+        while self.light_is_on():
+            self.set_light_off()
+            #go to the right and compare every element from the list with the robot item
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+                #if robot's item is bigger than the list item - swap
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    #the light is on, so while loop will work again
+                    self.set_light_on()
+            #the last right item is swapped
+            self.swap_item()
+            #go to the left and compare every element from the list with the robot item
+            while self.can_move_left():
+                self.swap_item()
+                self.move_left()
+                #if robot's item is smaler than the list item - swap
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    #the light is on, so while loop will work again
+                    self.set_light_on()
+            #the first left item is swapped
+            self.swap_item()
+#----------------first solution----------------
+    def sort_first_solution(self):
+        """
+        Sort the robot's list.
+        """
+        # Bubble sort O(n^2)
+        self.set_light_on()   
+        while self.light_is_on():
+            self.set_light_off()
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+                if self.compare_item() == 1 :
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    if not self.light_is_on():
+                        self.set_light_on()
+                else:
+                    self.move_left()
+                    self.swap_item()
+                self.move_right()
+            while self.can_move_left():
+                self.move_left()
 
-
+  
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    l1 =[10, 4, 8, 6]
     robot = SortingRobot(l)
+    robot_1 = SortingRobot(l1)
 
     robot.sort()
     print(robot._list)
+    robot_1.sort_first_solution()
+    print(robot_1._list)
